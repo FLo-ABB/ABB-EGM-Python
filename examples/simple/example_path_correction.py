98110dd4547746136ba936b1bd252da41bfa62ce
MrCybernetic <121680049+FLo-ABB@users.noreply.github.com>
2025-04-17 19:33:26 +0200

    init path correction exemple file

---
+++ examples/simple/example_path_correction.py
@@ -0,0 +1,17 @@
+from ABBRobotEGM import EGM
+
+# Robot Module #
+# MODULE MainModule
+#     VAR egmident egmID1;
+
+#     PROC main()
+#         !TODO
+#     ENDPROC
+# ENDMODULE
+
+
+def main() -> None:
+    """
+    TODO
+    """
+    pass
